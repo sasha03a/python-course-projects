3c9365f58dfca200b0df08233d1e0cbfc56d054f
sasha03a <112439791+sasha03a@users.noreply.github.com>
2026-03-14 19:17:14 +0100

    Update main.py

--- hangman/main.py
+++ hangman/main.py
@@ -1,13 +1,11 @@
 import random
 
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-
 from hangman_words import word_list
 from hangman_art import stages, logo
 
 lives = 6
 
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+
 print(logo)
 
 chosen_word = random.choice(word_list)
@@ -24,12 +22,11 @@ correct_letters = []
 
 while not game_over:
 
-    # TODO-6: - Update the code below to tell the user how many lives they have left.
+    
     print(f"****************************{lives}/6 LIVES LEFT****************************")
     guess = input("Guess a letter: ").lower()
 
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
+    
     display = ""
 
     for letter in chosen_word:
@@ -43,16 +40,14 @@ while not game_over:
 
     print("Word to guess: " + display)
 
-    # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-    #  e.g. You guessed d, that's not in the word. You lose a life.
-
+    
     if guess not in chosen_word:
         lives -= 1
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         if lives == 0:
             game_over = True
 
-            # TODO 7: - Update the print statement below to give the user the correct word they were trying to guess.
+       
             print(f"***********************YOU LOSE**********************")
             print(f"The word was {chosen_word}")
 
